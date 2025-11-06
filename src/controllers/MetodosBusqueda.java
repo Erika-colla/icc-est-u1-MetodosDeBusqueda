f5e4429854028d76dd2fc2ba7b8d477ff3c4904d
Erika-colla <collaguazobelen8@gmail.com>
2025-11-06 11:02:44 -0500

    feat(ups-001): Busqueda lineal

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -1,5 +1,68 @@
 package controllers;
 
+import views.ShowConsole;
+
 public class MetodosBusqueda {
 
+    private int[] arreglo;
+    private ShowConsole showConsole;
+
+    public MetodosBusqueda() {
+        System.out.println("\nEstudiante: Erika Collaguazo");
+        this.arreglo = new int[] {10, 11, -2, 5, 6, 8, 15, 22};
+        this.showConsole = new ShowConsole();
+        showConsole.printArray(arreglo);
+        int result1 = busquedaLineal(5);
+        showConsole.printResult(result1, 5);
+
+        int result2 = busquedaLinealWhile(-2);
+        showConsole.printResult(result2, -2);
+
+        int result3 = busquedaLinealWhile(20);
+        showConsole.printResult(result3, 20);
+
+        int result4 = busquedaLinelObj(10);
+        showConsole.printResult(result4, 10);
+
+        Integer result5 = busquedaLinelObj(100);
+        showConsole.printResult(result5);
+
+        Integer result6 = busquedaLinelObj(10);
+        showConsole.printResult(result6);
+
+    }
+
+    
+
+    public int busquedaLineal(int busqueda ) {
+        for (int i = 0; i < arreglo.length; i++) {
+           
+            if (arreglo[i] == busqueda) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int busquedaLinealWhile(int busqueda) {
+        int i = 0;
+        while(i < arreglo.length) {
+            if (arreglo[i] == busqueda) {
+                return i;
+            }
+            i++;
+
+        }
+        return -1; 
+    }
+
+    public Integer busquedaLinelObj(int value) {
+        for (int i = 0; i < arreglo.length; i++) {
+           
+            if (arreglo[i] == value) {
+                return arreglo[i];
+            }
+        }
+        return null;
+    }
 }
--- src/views/ShowConsole.java
+++ src/views/ShowConsole.java
@@ -2,4 +2,35 @@ package views;
 
 public class ShowConsole {
 
+    public ShowConsole(){
+
+    }
+
+    public void printArray(int[] numeros){
+        System.out.print("[");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i]);
+            if (i < numeros.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public void printResult(int result, int busqueda) {
+        if (result == -1) {
+             System.out.println("Error -> No se encontro el {" + busqueda + "}");
+        } else {
+             System.out.println("El numero {" + busqueda + "} se encontro en la posicion [" + result + "]");
+        }
+     }
+
+    public void printResult(Integer result) {
+        if(result != null) {
+            System.out.println("El {" + result + "} se encontro");
+        }else{
+            System.out.println("Error -> El numero buscado No se encontro ");
+        }
+    }
+
 }
